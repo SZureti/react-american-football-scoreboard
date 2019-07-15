3f5db86ded67d5f2e4e86673a2eb3ceb916aee51
SAIPHER ZURETI <saipherzureti@gmail.com>
2019-07-15 15:46:28 -0500

    added npm

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@
 import React from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
+// import React, { useState } from 'react';
 
 function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  //TODO: STEP 2 - Establish your application's state with some useState hooks.  You'll need one for the home score and another for the away score.
 
   return (
     <div className="container">
